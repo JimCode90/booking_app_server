60e8257a5fa07e2dec51bfd09b2db6c705433f3e
Subzer09 <jimmypaytan10@gmail.com>
2022-08-19 00:06:38 -0500

    commit

--- firebase/index.js
+++ firebase/index.js
@@ -1,6 +1,8 @@
 import admin from 'firebase-admin'
 const serviceAccount = require("./serviceAccountKey.json");
 
+
+
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
     databaseURL: "http://localhost:3000"
